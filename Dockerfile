67457ac136454a903057b94231188b0696e8ce8c
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-07-01 22:50:34 +0200

    testcontainer has healthcheck

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM httpd:2.4.43
 
-# RUN apt-get update && \
-#     apt-get -y install libcgi-session-perl liburi-escape-xs-perl gettext-base
+RUN apt-get update && \
+    apt-get -y install curl
+    # apt-get -y install libcgi-session-perl liburi-escape-xs-perl gettext-base
 
 COPY cgi/ /usr/local/apache2/cgi-bin/MyFavoriteThings/cgi
 COPY html/ /usr/local/apache2/cgi-bin/MyFavoriteThings/html/
@@ -16,6 +17,9 @@ COPY myfavCss/ /usr/local/apache2/cgi-bin/MyFavoriteThings/myfavCss/
 COPY dockerize/httpd.conf /usr/local/apache2/conf/httpd.conf
 
 RUN chmod u+x /usr/local/apache2/cgi-bin/MyFavoriteThings/cgi/*
+
+HEALTHCHECK --interval=2s --timeout=2s --retries=3 \
+  CMD curl --silent --fail localhost || exit 1
 
 # ENV DOCKER_INTERNAL_HTTP_PORT=80
 
--- test/installer_test.sh
+++ test/installer_test.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -eu 
 set -o pipefail
 
-CONTAINER_ID=$(docker run --publish 80:80 --detach myfavoritethings-test)
-echo "Testcontainer running with id $CONTAINER_ID"
+function is_healty {
+    local container_id=$1
+    test $(docker inspect --format='{{json .State.Health.Status}}' $container_id) = "\"healthy\""
+}
 
-sleep 5
+function block_until_available {
+    local container_id=$1
+    printf "Waiting for testcontainer to become healthy"
+    while ! is_healty $CONTAINER_ID; do
+        printf "."
+        sleep 1
+    done
+    printf "\n"
+}
+
+CONTAINER_ID=$(docker run --publish 80:80 --detach --rm myfavoritethings-test)
+echo "Testcontainer running with id $CONTAINER_ID"
+block_until_available $CONTAINER_ID
 
 echo "Killing testcontainer $CONTAINER_ID"
 docker rm -f $CONTAINER_ID
